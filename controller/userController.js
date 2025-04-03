279aa135d9b74c8ae8cd3b3e1b90e384a5eaa4ed
Sang Near <nguyenlamsang2004@gmail.com>
2025-04-03 15:32:49 +0700

    fix cors

--- controller/userController.js
+++ controller/userController.js
@@ -16,10 +16,10 @@ const login = async (req,res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
-            samesite: "none",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        })
+            secure: process.env.NODE_ENV === 'production', // Set `secure: true` chỉ khi production
+            sameSite: "None",  // Cho phép cookie được gửi trong các yêu cầu cross-site
+            maxAge: 7 * 24 * 60 * 60 * 1000,  // 7 ngày
+        });
 
         res.status(201).json({
             user: {
