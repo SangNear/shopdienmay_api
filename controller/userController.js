cdb86765d39d800699cddef507bfc96b7370b4fc
Sang Near <nguyenlamsang2004@gmail.com>
2025-04-03 15:27:39 +0700

    fix cors error

--- controller/userController.js
+++ controller/userController.js
@@ -17,7 +17,7 @@ const login = async (req,res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            samesite: "strict",
+            samesite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
